4ce3ecbbd458d86d62e1894f8827d8248bb906cb
rahul <rahulkhapre1998@gmail.com>
2022-10-23 15:00:49 +0530

    Refactored handler initialization code

--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -6,21 +6,34 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
 	"urlshort"
 )
 
-func readFile(filePath string) []byte {
-	yaml, err := ioutil.ReadFile(filePath)
+var validPath = regexp.MustCompile(".(yaml|json)$")
+
+func readConf(filePath string) ([]byte, string) {
+	m := validPath.FindStringSubmatch(filePath)
+	if m == nil {
+		log.Fatal("Unsupported filetype : " + filePath)
+	}
+	conf, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return yaml
+	return conf, m[1]
 }
 
+func readUrlShortParameters() (filePath *string) {
+	filePath = flag.String("conf", "../data/redirect.yaml", "configuration file to be used, .json and .yaml files are supported")
+	flag.Parse()
+	return
+}
 func main() {
 	mux := defaultMux()
-	filePath := flag.String("yaml", "../data/redirect.yaml", ".yaml configuration file")
-	flag.Parse()
+
+	filePath := readUrlShortParameters()
+
 	// Build the MapHandler using the mux as the fallback
 	pathsToUrls := map[string]string{
 		"/urlshort-godoc":      "https://godoc.org/github.com/gophercises/urlshort",
@@ -29,15 +42,14 @@ func main() {
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	// Build the jsonHandler using the mapHandler as the
-	// fallback
-	yamlHandler, err := urlshort.YAMLHandler(readFile(*filePath), mapHandler)
+	conf, ext := readConf(*filePath)
+	handler, err := urlshort.Handler(conf, ext, mapHandler)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", handler)
 }
 
 func defaultMux() *http.ServeMux {
