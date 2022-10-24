39364ff31e71bddd184bda995ad926dd8b32b9fa
rahul <rahulkhapre1998@gmail.com>
2022-10-24 22:51:41 +0530

    Refactored handler methods, updated template file

--- adventure/main/main.go
+++ adventure/main/main.go
@@ -2,33 +2,43 @@ package main
 
 import (
 	"adventure"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 )
 
-var templates = template.Must(template.ParseFiles("../tmpl/page.html"))
+var templates = template.Must(template.ParseFiles("../tmpl/view.html"))
 
 func handler(w http.ResponseWriter, r *http.Request, story adventure.StoryArc) {
-	err := templates.ExecuteTemplate(w, "page.html", story)
+	err := templates.ExecuteTemplate(w, "view.html", story)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-func makeHandler(fn func(http.ResponseWriter, *http.Request, adventure.StoryArc)) http.HandlerFunc {
+func viewHandler(fn func(http.ResponseWriter, *http.Request, adventure.StoryArc), storyMap map[string]adventure.StoryArc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		key := r.URL.Path[len("/view/"):]
-		fn(w, r, storyMap[key])
+		if v, ok := storyMap[key]; ok {
+			fn(w, r, v)
+		} else {
+			http.NotFound(w, r)
+		}
 	}
 }
 
-var storyMap = adventure.ReadStory("../data/gopher.json")
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/view/intro", http.StatusFound)
+}
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/view/intro", http.StatusFound)
-	})
-	http.HandleFunc("/view/", makeHandler(handler))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	storyMap := adventure.ReadStory("../data/gopher.json")
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/view/", viewHandler(handler, storyMap))
+	fmt.Println("Starting http server at port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
